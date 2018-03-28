9f68cfeafe239e6391db08c523594d30dabba495
paulboulescu <paul.boulescu@gmail.com>
2018-03-28 12:32:13 +0300

    Add card interaction

--- js/app.js
+++ js/app.js
@@ -77,20 +77,17 @@ function initialize() {
 	resetScore();
 }
 
+// add event listeners for all interactive elements
+function createInteraction(){
+	// select all cards from the DOM
+	let cards = document.querySelectorAll('.card');
+	// loop over each of the selected card elements
+	for (let card of cards){
+		// add event listeners for cards
+		card.addEventListener('click', openCard);
+	}
+}
+
 // initialize the game
 initialize();
-
-
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+createInteraction();
